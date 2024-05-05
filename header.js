62f3506386465f1c6b51ed0b46c5a79519accfa2
Dawson Butts <dawsontbutts@gmail.com>
2024-05-05 01:15:23 -0600

    Created header element that can be included on other pages.

--- header.js
+++ header.js
@@ -1,9 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const headerPlaceholder = document.getElementById('header-container');
+class CustomHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = '<div class="header">' +
+            '<div class="logo">PUT LOGO</div>' +
+            '<ul class="header-menu">' +
+            '<li class="menu-item"><a href="pages/about.html" class="menu-link">About</a></li>' +
+            '<li class="menu-item"><a href="pages/experience.html" class="menu-link">Experience</a></li>' +
+            '<li class="menu-item"><a href="pages/projects.html" class="menu-link">Projects</a></li>' +
+            '<li class="menu-item"><a href="pages/resume.html" class="menu-link">Resume</a></li>' +
+            '<li class="menu-item"><a href="pages/contact.html" class="menu-link">Contact</a></li>' +
+            '</ul>' +
+            '</div>';
+    }
+}
 
-    fetch('header.html')
-        .then(response => response.text())
-        .then(html => {
-            headerPlaceholder.innerHTML = html;
-        });
-});
+customElements.define("custom-header", CustomHeader);
