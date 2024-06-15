5875ea7f31d6af355acf393efbd83392390d1219
Dawson Butts <dawsontbutts@gmail.com>
2024-06-15 16:33:55 -0600

    Created header functionality for all pages + added .css for all pages I need.

--- header.js
+++ header.js
@@ -1,15 +1,27 @@
 class CustomHeader extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = '<div class="header">' +
-            '<div class="logo">PUT LOGO</div>' +
-            '<ul class="header-menu">' +
-            '<li class="menu-item"><a href="pages/about.html" class="menu-link">About</a></li>' +
-            '<li class="menu-item"><a href="pages/experience.html" class="menu-link">Experience</a></li>' +
-            '<li class="menu-item"><a href="pages/projects.html" class="menu-link">Projects</a></li>' +
-            '<li class="menu-item"><a href="pages/resume.html" class="menu-link">Resume</a></li>' +
-            '<li class="menu-item"><a href="pages/contact.html" class="menu-link">Contact</a></li>' +
-            '</ul>' +
-            '</div>';
+
+        // get html content from the file 
+        fetch('/header.html')
+            .then(response => response.text()) // convert the page to text 
+            .then(data => {
+                this.innerHTML = data; // insert the fetched text to this page
+                this.addEventListenerToLogo();  // add listener to home
+            })
+            .catch(error => console.error("Error fetching header html"))
+    }
+
+    addEventListenerToLogo() {
+        const logo = this.querySelector('.logo');
+        if (logo) {
+            logo.addEventListener('click', this.handleLogoClick);
+        }
+    }
+
+    // handle logo click
+    handleLogoClick(event) {
+        event.preventDefault(); // Prevent the default action if needed
+        window.location.href = '/';
     }
 }
 
