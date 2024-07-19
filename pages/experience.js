178c627b25771bac3643f97232e755e367d7435e
Dawson Butts <dawsontbutts@gmail.com>
2024-07-19 17:52:32 -0600

    Updated resume.

--- pages/experience.js
+++ pages/experience.js
@@ -5,8 +5,8 @@ document.getElementById("downResBut").addEventListener("click", resumeDownloadCl
 
 function resumeDownloadClick() {
     var resumeFile = document.createElement('a');
-    resumeFile.href = "/assets/butts_dawson_resume.pdf";
-    resumeFile.download = "butts_dawson_resume.pdf";
+    resumeFile.href = "/assets/butts_dawson_resume_fin.pdf";
+    resumeFile.download = "butts_dawson_resume_fin.pdf";
     document.body.appendChild(resumeFile);
     resumeFile.click();
     document.body.removeChild(resumeFile);
